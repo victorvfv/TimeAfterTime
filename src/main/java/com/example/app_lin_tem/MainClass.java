b89abe78e6862b56888758cb418281e040b44d77
victorvfv <149057094+victorvfv@users.noreply.github.com>
2026-05-17 21:39:22 +0200

    Por fin le pude poner icono a la aplicacion pero por alguna razon no se muestra en la barra de tareas solo en la app

--- src/main/java/com/example/app_lin_tem/MainClass.java
+++ src/main/java/com/example/app_lin_tem/MainClass.java
@@ -4,16 +4,20 @@ import com.example.app_lin_tem.Controllers.MainViewController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 
 public class MainClass extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+        Image iconImage = new Image(new FileInputStream("src/main/resources/Iconos/reloj.png"));
         FXMLLoader fxmlLoader = new FXMLLoader(MainClass.class.getResource("Main-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("TimeAfterTime");
+        stage.getIcons().add(iconImage);
         stage.setResizable(true);
         stage.setMaximized(true);
         stage.setScene(scene);
